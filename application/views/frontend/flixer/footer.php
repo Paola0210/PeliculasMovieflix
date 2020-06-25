221bf6a7e72ac984e0ba8e61c08e55426931111c
yesidvestel <58863813+yesidvestel@users.noreply.github.com>
2020-06-25 16:55:50 -0500

    iconos canales

--- application/views/frontend/flixer/footer.php
+++ application/views/frontend/flixer/footer.php
@@ -2,11 +2,12 @@
 	<div class="row">
 		<div class="col-lg-12">
 			<ul class="list-unstyled">
-				<li><a href="<?php echo base_url();?>index.php?general/faq">Faq</a></li>
-				<li><a href="<?php echo base_url();?>index.php?general/privacypolicy">Privacy Policy</a></li>
-				<li><a href="<?php echo base_url();?>index.php?general/refundpolicy">Refund Policy</a></li>
+				<li><a href="<?php echo base_url();?>index.php?general/faq">PQR</a></li>
+				<li><a href="<?php echo base_url();?>index.php?general/privacypolicy">Politicas de privacidad</a></li>
+				<li><a href="<?php echo base_url();?>index.php?general/refundpolicy">Politicas de Reembolso</a></li>
 			</ul>
-			<p><a href="https://www.facebook.com/MikrotikColombia/" rel="nofollow"> Colombia - Vesflix</a>. <br>e-mail <a href="mailto:canal6yopal@gmail">canal6yopal@gmail.com</a>.</p>
+			<p><a href="https://www.facebook.com/MikrotikColombia/" rel="nofollow"> TvDIGITAL </a>. <br>
+			<a href="mailto:canal6yopal@gmail">tvdigital305@gmail.com</a>.</p>
 		</div>
 	</div>
 </footer>
